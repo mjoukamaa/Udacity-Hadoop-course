f18427e2cc4fbd0c9c590b267076bef41253a92a
Mauno Joukamaa <44747647+mjoukamaa@users.noreply.github.com>
2018-11-05 17:54:47 +0200

    Update sum_reducer.py

--- sum_reducer.py
+++ sum_reducer.py
@@ -2,23 +2,33 @@
 
 import sys
 
+# Initialize variables for holding keys and values
 valueTotal = 0
 oldKey = None
 
+# Loop through key-value pairs
 for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) != 2:
-        # Something has gone wrong. Skip this line.
+        # Something has gone wrong, skip this line
         continue
 
     thisKey, thisValue = data
+    
+    # Note when reducer moves to new key
     if oldKey and oldKey != thisKey:
+        
+        # Print total sum for previous key
         print("{0}\t{1}".format(oldKey, valueTotal))
+        
+        # Reset valueTotal for new key
         oldKey = thisKey
         valueTotal = 0
 
+    # Process current key-value pair
     oldKey = thisKey
     valueTotal += float(thisValue)
 
+# When exiting loop print total sum for last key
 if oldKey != None:
     print("{0}\t{1}".format(oldKey, valueTotal))
