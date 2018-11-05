b8c503c0269018f0a597189f7cca458ba6864249
Mauno Joukamaa <44747647+mjoukamaa@users.noreply.github.com>
2018-11-05 18:01:29 +0200

    Add files via upload

---
+++ index_mapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import re
+
+# Define characters to split text on
+split_chars = re.compile(r'[\s\.,!\?:;"\(\)<>\[\]#\$=\-/]')
+
+# Read in input to be processed and loop through it
+reader = csv.reader(sys.stdin, delimiter='\t')
+
+for line in reader:
+
+    # Split text part of forum post (line[4]) and print
+    # each splitted part with forum post id (line[0])
+    try:
+        for word in (re.split(split_chars, line[4])):
+            print("{0}\t{1}".format(word.lower(), line[0]))
+
+    # Ignore lines that don't split as expected
+    except:
+        continue
