0d5192fc1a3af706fbc4b1dfa14ed39c0dace535
Mauno Joukamaa <44747647+mjoukamaa@users.noreply.github.com>
2018-11-15 22:51:28 +0200

    Update post_length_mapper.py

--- post_length_mapper.py
+++ post_length_mapper.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 """
-This mapper reads through forum posts of predefined format, determines whether
-they are questions (ie. starting posts of thread) or other posts, then outputs
-the thread id, post type, and post length in characters.
+Mapper reads through forum posts of predefined format, determines if they
+are questions (ie. starting posts of thread) or other posts, then
+outputs thread id, post type, and post length in characters.
 """
 
 import sys
@@ -14,6 +14,7 @@ reader = csv.reader(sys.stdin, delimiter='\t')
 
 for line in reader:
 
+    # Check to see if post is question, output id, type and length if so
     if line[5] == 'question':
         question_id = line[0]
         post_type = line[5]
@@ -21,6 +22,7 @@ for line in reader:
 
         print("{0}\t{1}\t{2}".format(question_id, post_type, post_length))
 
+    # Otherwise if post is answer, output id of its parent question, type and length
     elif line[5] == 'answer':
         question_id = line[6]
         post_type = line[5]
@@ -28,5 +30,6 @@ for line in reader:
 
         print("{0}\t{1}\t{2}".format(question_id, post_type, post_length))
 
+    # Disregard other types of posts
     else:
         continue
