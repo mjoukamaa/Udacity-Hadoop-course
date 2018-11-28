dcad9967ac55ce084fb1af03491a165bec30c705
Mauno Joukamaa <44747647+mjoukamaa@users.noreply.github.com>
2018-11-28 13:03:05 +0200

    Update index_reducer.py

--- index_reducer.py
+++ index_reducer.py
@@ -2,24 +2,34 @@
 
 import sys
 
+# Initialize variables for holding keys and values
 nodeIds = []
 oldKey = None
 
+# Loop through key-value pairs
 for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) != 2:
-        # Something has gone wrong. Skip this line.
+        # Something has gone wrong, skip this line
         continue
 
     thisKey, thisValue = data
+    
+    # Note when reducer moves to new key
     if oldKey and oldKey != thisKey:
 
+        # Determine how many times current word 
+        # appears (incl. multiple hits in one post)
         numberHits = len(nodeIds)
+        # Use set() to get only distinct forum posts
+        # current word appears in
         distinctIds = set(nodeIds)
+        # Sort distinct posts for more usable index
         distinctIds = sorted(list(distinctIds))
 
         print("{0}\t{1}\t{2}".format(oldKey, numberHits, distinctIds))
 
+        # Reset nodeIds for new key
         oldKey = thisKey
         nodeIds = []
 
@@ -32,6 +42,8 @@ for line in sys.stdin:
     except:
         continue
 
+
+# When exiting loop, perform index building process for last word
 if oldKey != None:
 
     numberHits = len(nodeIds)
